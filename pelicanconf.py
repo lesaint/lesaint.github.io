177ccda56fb934dd8869679d0775d709b6d588a2
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2024-06-03 14:33:17 +0200

    keep CNAME file at root of directory

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,6 +8,12 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en'
 
+# static resources
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {
+    'extra/CNAME': {'path': 'CNAME'},
+}
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
